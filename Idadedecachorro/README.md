38bf48103cf1f5aab7627670622a2e9156de553e
Murilo Eduardo Alves Vaz <murilo.near@gmail.com>
2018-06-27 13:01:15 -0300

    App 03 - Idade de cachorro

---
+++ Idadedecachorro/README.md
@@ -0,0 +1,3 @@
+#App03
+Idade de cachorro
+App que faz a conversão da idade do cachorro para anos humanos.
---
+++ Idadedecachorro/app/src/main/java/idadedecachorro/cursoandroid/com/idadedecachorro/MainActivity.java
@@ -0,0 +1,40 @@
+package idadedecachorro.cursoandroid.com.idadedecachorro;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText caixaTexto;
+    private Button botaoIdade;
+    private TextView resultadoIdade;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        caixaTexto = findViewById(R.id.caixaTextoId);
+        botaoIdade = findViewById(R.id.botaoIdadeId);
+        resultadoIdade = findViewById(R.id.resultadoIdadeId);
+
+        botaoIdade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String textoDigitado = caixaTexto.getText().toString();
+                if (textoDigitado.isEmpty()) {
+                    resultadoIdade.setText("Nenhum número digitado !!");
+                } else {
+                    int valorDigitado = Integer.parseInt(textoDigitado);
+                    int ValorResultadoFinal = valorDigitado * 7;
+                    resultadoIdade.setText("A idade do cachorro em anos humanos é: " + ValorResultadoFinal + " anos ");
+
+                }
+            }
+        });
+    }
+}
