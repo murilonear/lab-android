9c7b1cadeb9ac685b48d2da1600c445c7546719e
Murilo Eduardo Alves Vaz <murilo.near@gmail.com>
2018-06-29 10:57:35 -0300

    App 05 - Atm Empresa Consultoria

--- AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/ClienteActivity.java
+++ AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/ClienteActivity.java
@@ -10,5 +10,6 @@ public class ClienteActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cliente);
+        getSupportActionBar().hide();
     }
 }
--- AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/ServicoActivity.java
+++ AtmEmpresaConsultoria/app/src/main/java/atmempresaconsultoria/cursoandroid/com/atmempresaconsultoria/ServicoActivity.java
@@ -10,5 +10,6 @@ public class ServicoActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servico);
+        getSupportActionBar().hide();
     }
 }
